7c221963a5c5afa30cd53195b17b720c69eee582
Aleksei <aleksei.golubenko@yandex.ru>
2023-10-30 19:51:38 +0300

    Доделал основу main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -11,41 +11,27 @@ class MainPage(Base):
         self.driver = driver
 
     # Locators
-    product_1 = "//input[@id='user-name']"
-    cart = "//a[@class='shopping_cart_link']"
+    add_to_cart_product_1 = "//button[@id='add-to-cart-sauce-labs-backpack']"
+    cart = "//div[@id='shopping_cart_container']"
 
     # Getters
-    def get_user_name(self):
-        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.user_name)))
+    def get_add_to_cart_product_1(self):
+        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.add_to_cart_product_1)))
 
-    def get_password(self):
-        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.password)))
-
-    def get_login_button(self):
-        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.login_button)))
-
-    def get_main_word(self):
-        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.text_main_page)))
-    # но слово вряд ли может быть кликабельным. хотя как поосмотреть...
+    def get_cart(self):
+        return WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, self.cart)))
 
     # Actions
-    def input_user_name(self, user_name):
-        self.get_user_name().send_keys(user_name)
-        print("Ввод имени пользователя.")
+    def click_add_to_cart_product_1(self):
+        self.get_add_to_cart_product_1().click()
+        print("Добавление в корзину товара 'Sauce Labs Backpack' (первый товар).")
 
-    def input_password(self, password):
-        self.get_password().send_keys(password)
-        print("Ввод пароля.")
-
-    def click_login_button(self):
-        self.get_login_button().click()
-        print("Нажатие на кнопку Login.")
+    def go_to_cart(self):
+        self.get_cart().click()
+        print("Переход в корзину.")
 
     # Methods
-    def authorization(self):
-        self.driver.get(self.url_login_page)
+    def add_to_cart_product(self):
+        self.click_add_to_cart_product_1()
+        self.go_to_cart()
         self.get_current_url()
-        self.input_user_name('standard_user')
-        self.input_password('secret_sauce')
-        self.click_login_button()
-        self.get_assert_text(self.get_main_word(), 'Products')
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
-def test_buy_product():
+def test_buy_product_1():
     o = Options()
     o.add_experimental_option("detach", True)
     o.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -15,8 +13,5 @@ def test_buy_product():
     login = LoginPage(driver)
     login.authorization()
 
-    # Переход в корзину
-    shopping_cart = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH,
-                                                                               "//a[@class='shopping_cart_link']")))
-    shopping_cart.click()
-    print("Переход в корзину.")
+    add_and_go_to_cart = MainPage(driver)
+    add_and_go_to_cart.add_to_cart_product()
